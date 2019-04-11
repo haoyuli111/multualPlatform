ee5f272357db9227a00277e925df6ad20d47fa5e
haoyuli <1065618302@qq.com>
2019-04-11 12:53:53 +0800

    提交team相关entity

---
+++ src/main/java/cn/lichuachua/mp/mpserver/entity/Team.java
@@ -0,0 +1,59 @@
+package cn.lichuachua.mp.mpserver.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * @author 李歘歘
+ */
+@Data
+@Entity(name = "mp_team")
+public class Team {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid",strategy = "uuid")
+    @Column(name = "team_id")
+    private String teamId;
+
+    @Column(name = "team_name")
+    private String teamName;
+
+    @Column(name = "header_id")
+    private String headerId;
+
+    @Column(name = "header_nick")
+    private String headerNick;
+
+    @Column(name = "describe")
+    private String describe;
+
+    @Column(name = "header_avatar")
+    private String headerAvatar;
+
+    @Column(name = "header_mobile")
+    private String headerMobile;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private Integer status;
+
+    @Column(name = "visual")
+    private Integer visual;
+
+    @Column(name = "type")
+    private Integer type;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
